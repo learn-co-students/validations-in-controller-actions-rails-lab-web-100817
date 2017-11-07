77d000cdd23363a4f380b6c3a719a6d1994124b7
Gonzodamus <Dick@TheDickWard.com>
2017-11-07 15:37:48 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'byebug'
 
 group :development, :test do
   gem 'pry'
@@ -15,4 +16,3 @@ end
 group :development do
   gem 'web-console', '~> 2.0'
 end
-
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 100}
+  validate :is_fiction_or_nonfiction
+
+
+  def is_fiction_or_nonfiction
+    # byebug
+    unless category == "Fiction" || category == "Non-Fiction"
+      errors.add(:category, "is not a valid category.")
+    end
+  end
+
 end
